e8375244972c6fa1c4d9dd7d7e92a7991b0e59a8
luis-procopio <luisprocopioarruda@gmail.com>
2024-08-26 01:49:52 -0300

    lsp config

--- init.lua
+++ init.lua
@@ -15,7 +15,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error == 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -26,6 +26,12 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  ui = {
+    border = "rounded",
+  },
+  install = {
+    colorscheme = { "catppuccin" },
+  },
   spec = {
     { import = "plugins" },
   },
---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,30 @@
+return {
+	{
+		"williamboman/mason.nvim",
+
+		config = function()
+			require("mason").setup()
+		end,
+	},
+
+	{
+		"neovim/nvim-lspconfig",
+
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+					},
+				},
+			})
+		end,
+	},
+}
